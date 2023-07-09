09afa549123911193ba2d9516b24e0793c0cda60
Michael <m@michaelti.ca>
2023-07-09 13:02:05 +0100

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main>
+	<h1>
+		{$page.error.message}
+		({$page.status})
+	</h1>
+	<a href="/">Back to Timer</a>
+</main>
+
+<style>
+	main {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	a {
+		color: #fff;
+	}
+</style>
